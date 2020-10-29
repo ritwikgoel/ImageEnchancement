2026a238d02aa3d1691a0331b204aa9790ceedff
tangobeer <ritwikgoel@yahoo.com>
2020-10-29 19:55:40 +0530

    Third last commit lol

--- Final/mainscripttesting.m
+++ Final/mainscripttesting.m
@@ -87,27 +87,65 @@ for k = 1:6
 %   M = uint8(SMQT(double(imageData),1,8));
 %   lowlight=lowlight2(M);
 %   figure; imshow(lowlight);
-    if (imcounterSMQT<= imcounterweiner) && (imcounterSMQT<=imcounterlowlight)
+    if (psnrcounterSMQT<= psnrcounterweiner) && (psnrcounterSMQT<=psnrcounterlowlight)
         %SMQT lowest
         weiner=weinerFinal(imageData);
         lowlight=lowlight2(weiner);
         montage({imageData,lowlight})
-        title('Immse');
+        title('Hybrid of best 2 algorithm with respect to PSNR (weiner->lowlight)')
        
-    elseif (imcounterSMQT>= imcounterweiner) && (imcounterweiner<=imcounterlowlight)
+    elseif (psnrcounterSMQT>= psnrcounterweiner) && (psnrcounterweiner<=psnrcounterlowlight)
         %weiner lowest lowlight and weiner 
         M = uint8(SMQT(double(imageData),1,8));
         lowlight=lowlight2(M);
         montage({imageData,lowlight})
-        title('Immse')
+        title('Hybrid of best 2 algorithm with respect to PSNR (SMQT->lowlight)')
         
-    elseif (imcounterlowlight<=imcounterweiner) && (imcounterSMQT>=imcounterlowlight)
+    elseif (psnrcounterlowlight<=psnrcounterweiner) && (psnrcounterSMQT>=psnrcounterlowlight)
         %lowlight lowest smqt and weiner 
         M = uint8(SMQT(double(imageData),1,8));
         weiner=weinerFinal(M);
         montage({imageData,weiner})
-        title('Immse')
+        title('Hybrid of best 2 algorithm with respect to PSNR (SMQT->weiner)')
+    end
+    %montage({imageData,finalimage}) 
+    pause(1);
         
+end
+
+for k = 1:6
+  jpgFilename = sprintf('%d.jpeg', k);
+  fullFileName = fullfile('/home/tangobeer/Desktop/ImageEnchancement/DataSet/car', jpgFilename);
+  if exist(fullFileName, 'file')
+    imageData = imread(fullFileName );
+    imageDatagray=rgb2gray(imageData);
+  else
+    warningMessage = sprintf('Warning: image file does not exist:\n%s', fullFileName);
+    uiwait(warndlg(warningMessage));
+  end
+%   M = uint8(SMQT(double(imageData),1,8));
+%   lowlight=lowlight2(M);
+%   figure; imshow(lowlight);
+    if (bricounterSMQT<= bricounterweiner) && (bricounterSMQT<=bricounterlowlight)
+        %SMQT lowest
+        weiner=weinerFinal(imageData);
+        lowlight=lowlight2(weiner);
+        montage({imageData,lowlight})
+        title('Hybrid of best 2 algorithm with respect to Brisk (weiner->lowlight)')
+       
+    elseif (bricounterSMQT>= bricounterweiner) && (bricounterweiner<=bricounterlowlight)
+        %weiner lowest lowlight and weiner 
+        M = uint8(SMQT(double(imageData),1,8));
+        lowlight=lowlight2(M);
+        montage({imageData,lowlight})
+        title('Hybrid of best 2 algorithm with respect to Brisk (SMQT->lowlight)')
+        
+    elseif (bricounterlowlight<=bricounterweiner) && (bricounterSMQT>=bricounterlowlight)
+        %lowlight lowest smqt and weiner 
+        M = uint8(SMQT(double(imageData),1,8));
+        weiner=weinerFinal(M);
+        montage({imageData,weiner})
+        title('Hybrid of best 2 algorithm with respect to Brisk (SMQT->weiner)')
     end
     %montage({imageData,finalimage}) 
     pause(1);
@@ -128,30 +166,32 @@ for k = 1:6
 %   M = uint8(SMQT(double(imageData),1,8));
 %   lowlight=lowlight2(M);
 %   figure; imshow(lowlight);
-    if (psnrcounterSMQT<= psnrcounterweiner) && (psnrcounterSMQT<=psnrcounterlowlight)
+    if (imcounterSMQT<= imcounterweiner) && (imcounterSMQT<=imcounterlowlight)
         %SMQT lowest
         weiner=weinerFinal(imageData);
         lowlight=lowlight2(weiner);
         montage({imageData,lowlight})
-        title('psnr')
+        title('Hybrid of best 2 algorithm with respect to IMMSE (weiner->lowlight)');
        
-    elseif (psnrcounterSMQT>= psnrcounterweiner) && (psnrcounterweiner<=psnrcounterlowlight)
+    elseif (imcounterSMQT>= imcounterweiner) && (imcounterweiner<=imcounterlowlight)
         %weiner lowest lowlight and weiner 
         M = uint8(SMQT(double(imageData),1,8));
         lowlight=lowlight2(M);
         montage({imageData,lowlight})
-        title('psnr')
+        title('Hybrid of best 2 algorithm with respect to IMMSE (SMQT->lowlight)')
         
-    elseif (psnrcounterlowlight<=psnrcounterweiner) && (psnrcounterSMQT>=psnrcounterlowlight)
+    elseif (imcounterlowlight<=imcounterweiner) && (imcounterSMQT>=imcounterlowlight)
         %lowlight lowest smqt and weiner 
         M = uint8(SMQT(double(imageData),1,8));
         weiner=weinerFinal(M);
         montage({imageData,weiner})
-        title('psnr')
+        title('Hybrid of best 2 algorithm with respect to IMMSE (SMQT->weiner)')
+        
     end
     %montage({imageData,finalimage}) 
     pause(1);
         
 end
+
 
 
